d23a0a1f8e9f68661aed95cae7e0e2b302788440
JJJoonngg <whdtls3878@gmail.com>
2022-07-21 22:27:32 +0900

    Initial Commit

---
+++ summer_coding_android/app/src/main/java/kr/co/landvibe/summer_coding_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.landvibe.summer_coding_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
