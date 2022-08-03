601f1fd984bb31d6e78d7130c4fc22b1127f2f51
김동하 <d11210920@gmail.com>
2022-08-03 21:02:06 +0900

    2weeks project

--- summer_coding_android/app/src/main/java/kr/co/landvibe/summer_coding_android/d11210920/D11210920Activity.kt
+++ summer_coding_android/app/src/main/java/kr/co/landvibe/summer_coding_android/d11210920/D11210920Activity.kt
@@ -1,12 +1,41 @@
 package kr.co.landvibe.summer_coding_android.d11210920
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import kr.co.landvibe.summer_coding_android.R
+import kr.co.landvibe.summer_coding_android.databinding.ActivityD11210920Binding
 
 class D11210920Activity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityD11210920Binding
+    private lateinit var activityResultLauncher : ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_d11210920)
+        binding = ActivityD11210920Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initView()
     }
+
+    private fun initView(){
+        binding.run{
+            dhSend.setOnClickListener {
+                val intent = Intent(this@D11210920Activity,D112109202Activity::class.java).apply {  }
+                intent.putExtra("money",dhHowmuch.text)
+                activityResultLauncher.launch(intent)
+            }
+
+            activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {result ->
+                if(result.resultCode == RESULT_OK){
+                    val receive_money:String? = result.data!!.getStringExtra("newMoney")
+                    dhHowmuch.setText(receive_money)
+                }
+            }
+
+        }
+    }
+
 }
