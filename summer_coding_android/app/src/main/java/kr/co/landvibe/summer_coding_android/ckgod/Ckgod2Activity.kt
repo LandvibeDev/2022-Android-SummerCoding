54a2f5a66140836156df22d086c2cb829058ed14
chako <chako@gmarket.com>
2022-08-04 21:17:50 +0900

    init week3

--- summer_coding_android/app/src/main/java/kr/co/landvibe/summer_coding_android/ckgod/Ckgod2Activity.kt
+++ summer_coding_android/app/src/main/java/kr/co/landvibe/summer_coding_android/ckgod/Ckgod2Activity.kt
@@ -1,25 +1,17 @@
 package kr.co.landvibe.summer_coding_android.ckgod
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kr.co.landvibe.summer_coding_android.R
+import androidx.appcompat.app.AppCompatActivity
 import kr.co.landvibe.summer_coding_android.databinding.ActivityCkgod2Binding
 
 class Ckgod2Activity : AppCompatActivity() {
-
-    val s: String? by lazy {
-        "ddd"
-    }
-
     private lateinit var binding: ActivityCkgod2Binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCkgod2Binding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.tvTmp.text = intent.getStringExtra("keyName")
 
         binding.btnTmp.setOnClickListener {
             val intent = Intent(this@Ckgod2Activity, CkgodActivity::class.java)
